f245f5e8373e5f6f02a5b0d5f49c1f3a668f4b7d
pbyanjankar <Prabin.Byanjankar@gmail.com>
2018-10-08 10:42:13 +0545

    #2 Upgrade angular core 6.1.2

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutModule } from '@angular/cdk/layout';
+import { HeaderComponent } from './layout/header/header.component';
+import { ParticipantComponent } from './layout/participant/participant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ParticipantComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
